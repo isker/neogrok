3e883650124a551f3d8c5d67d2423b4846fcbccb
Ian Kerins <ianskerins@gmail.com>
2022-08-26 23:43:46 -0400

    Functioning typescript and react

---
+++ src/app.tsx
@@ -0,0 +1,5 @@
+import * as styles from "./app.module.css";
+
+export function App() {
+  return <h1 className={styles.header}>Hello world!</h1>;
+}
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
+
+const app = document.getElementById("app")!;
+const root = createRoot(app);
+root.render(<App />);
