c883c6240534c11965919dcc6f3586a05c2178b5
Ian Kerins <git@isk.haus>
2023-07-10 00:18:42 -0400

    OpenGrok /diff compatibility

--- src/routes/(opengrok-compat)/diff/+page.svelte
+++
@@ -1,5 +0,0 @@
-<!--
-  Diff: probably will never support. Needs SCM integration that
-  zoekt does not have.
-  https://opengrok.libreoffice.org/diff/core/xmlsecurity/CppunitTest_qa_certext.mk?r1=%2Fcore%2Fxmlsecurity%2FCppunitTest_qa_certext.mk%402b383d19&r2=%2Fcore%2Fxmlsecurity%2FCppunitTest_qa_certext.mk%40de030cd7
--->
